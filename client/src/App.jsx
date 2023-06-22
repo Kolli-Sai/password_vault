b2762a9e9656927a3bee513c4d8e9127fb11db46
KolliSai <saik98187@gmail.com>
2023-06-22 10:43:28 +0530

    completed Homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home";
+import DashboardPage from "./pages/Dashboard";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
