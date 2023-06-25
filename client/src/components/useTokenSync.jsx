9abfb00b9601e22b38242c524857cb97d1cc1da6
KolliSai <saik98187@gmail.com>
2023-06-25 21:21:25 +0530

    corrected the path

--- client/src/components/useTokenSync.jsx
+++ client/src/components/useTokenSync.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { setToken } from "./store/isAuthenticated/isAuthenticatedSlice";
+import { setToken } from "../store/isAuthenticated/isAuthenticatedSlice";
 import Cookies from "js-cookie";
 
 export function useTokenSync() {
