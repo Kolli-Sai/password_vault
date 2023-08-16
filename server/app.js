eaff855c2411bfd0556e862b82be2cb031ce417e
KolliSai <saik98187@gmail.com>
2023-08-16 10:38:54 +0530

    added new origin in cors

--- server/app.js
+++ server/app.js
@@ -27,6 +27,7 @@ const corsOptions = {
     "https://password-vault-server-eq9u.onrender.com",
     "https://passwords-fyt5f2efk-kolli-sai.vercel.app",
     "https://passwords-kolli-sai.vercel.app/",
+    "https://passwords-o8kli05ex-kolli-sai.vercel.app/",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
